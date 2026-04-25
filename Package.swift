bb55ae27975183e71377373f13fc373f9507e193
Manuel Beck <manuelbeck87@outlook.de>
2026-04-25 14:42:55 +0200

    feat(ios): use SPM for Maps SDK since cordova-ios 8
    - When using cordova-ios 8 SPM (Swift Package Manager) will be used to use the Google Maps SDK dependency instead of CocoaPods
    - To get this working correctly minimum cordova-ios 8.0.1 has to be used
    - When using cordova-ios 7 CocoaPods will still be used

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "cordova-plugin-google-maps-sdk",
+    platforms: [.iOS(.v16)],
+    products: [
+        .library(name: "cordova-plugin-google-maps-sdk", targets: ["cordova-plugin-google-maps-sdk"])
+    ],
+    dependencies: [
+        // This must be included as a dependency, with this format for it to work.
+        .package(url: "https://github.com/apache/cordova-ios.git", branch: "master"),
+        .package(url: "https://github.com/googlemaps/ios-maps-sdk", exact: "10.8.0")
+    ],
+    targets: [
+        .target(
+            name: "cordova-plugin-google-maps-sdk",
+            dependencies: [
+                .product(name: "Cordova", package: "cordova-ios"),
+                .product(name: "GoogleMaps", package: "ios-maps-sdk")
+            ],
+            path: "src/ios",
+            resources: [],
+            publicHeadersPath: "."
+        )
+    ]
+)
